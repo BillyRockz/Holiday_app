258f4594b094ee600e007b972c4be3bdf3493a1a
Guillermo <guillermopr95@gmail.com>
2023-07-23 22:31:53 -0500

    V1 Added home screen and api

---
+++ run.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+from app import app
+
+if __name__ == "__main__":
+    app.run()
+
